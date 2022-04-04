3d011d1cfb2108cc3c12a13ef7c2892c376b971a
klastill <gyb0501@gmail.com>
2022-04-04 17:57:14 +0900

    mod isInSlice() => replace interface{} with generic

--- isInSlice.go
+++ isInSlice.go
@@ -2,13 +2,10 @@ package convpack
 
 import "reflect"
 
-func IsInSlice(element interface{}, slice interface{}) bool {
-	slice_itf := reflect.ValueOf(slice)
-	if slice_itf.Kind() == reflect.Slice {
-		for i := 0; i < slice_itf.Len(); i++ {
-			if slice_itf.Index(i).Interface() == element {
-				return true
-			}
+func IsInSlice[T any](element T, slice []T) bool {
+	for _, target := range slice {
+		if reflect.ValueOf(target).Interface() == reflect.ValueOf(element).Interface() {
+			return true
 		}
 	}
 	return false
